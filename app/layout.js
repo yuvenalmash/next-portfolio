180b8e9a67aac79d693687b93296ee7920085763
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-08 02:28:06 +0300

    Install fontawesome

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import Navbar from '@/components/navbar'
+
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         {children}
       </body>
     </html>
