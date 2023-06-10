dfbb9ce430f13151cff4091d5952fdcbb301635d
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-10 03:07:55 +0300

    Update styling

--- components/footer.js
+++ components/footer.js
@@ -1,26 +1,28 @@
+import React from "react";
 import { socialLinks } from "./portfolioData";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () => {
-  const logo = "/logo.svg";
+  const socialsMap = {
+    Github: AiOutlineGithub,
+    LinkedIn: AiOutlineLinkedin
+  };
+
   return (
     <footer className="w-screen h-12 bg-transparent flex justify-center items-center">
       <div className="flex justify-between items-center w-11/12">
         <div className="flex justify-center items-center">
-          <div className="border rounded-full border-white transition-colors duration-300 ease-in-out hover:text-red-500">
-            <img src={logo} alt="logo" className="h-8 w-8 p-1" />
+          <div className="border rounded-full border-white hover:scale-110 hover:animate-pulse">
+            <img src="/images/logo.svg" alt="logo" className="h-8 w-8 p-1" />
           </div>
           <p className="text-white text-lg font-bold ml-2">Yuvenal Njoroge</p>
         </div>
         <div className="flex justify-center items-center">
-          <a href={socialLinks.github} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} className="text-white text-2xl mr-4" />
-          </a>
-          <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl" />
-          </a>
+          {Object.keys(socialsMap).map((key, index) => (
+            <a key={index} href={socialLinks[key]} target="_blank" rel="noopener noreferrer">
+              {React.createElement(socialsMap[key], { className: "text-white text-2xl mr-4 transition-colors duration-300 ease-in-out hover:text-cyan-500" })}
+            </a>
+          ))}
         </div>
       </div>
     </footer>
--- components/projects.js
+++ components/projects.js
@@ -1,4 +1,3 @@
-// list projects
 import { projectsList } from "./portfolioData"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -6,9 +5,9 @@ import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = () => {
   return (
-    <div id="projects" className="py-20">
+    <div id="projects" className="py-20 flex flex-col justify-center items-center">
+      <h2 className="text-5xl font-retro_computer text-white text-center mb-16 py-3 px-5 border">Projects</h2>
       <div className="container mx-auto">
-        <h2 className="text-4xl font-bold text-white text-center mb-10">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {projectsList.map((project, index) => (
             <div key={index} className="bg-gradient-to-b from-transparent to-cyan-800 rounded-lg shadow-lg">
