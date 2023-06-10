427109743d7fe6cca979bf229f841252963fed49
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-10 17:40:42 +0300

    Change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        retro_computer: ['Retro_computer', 'sans-serif'],
+        boxpixies: ['Boxpixies', 'sans-serif'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 60% 60%, var(--tw-gradient-stops))',
+        'header-img': 'url(/images/header-img.png)',
       },
     },
   },
