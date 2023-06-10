bd79be22dd314915d57a037e4c417b4986ff6717
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-10 03:06:42 +0300

    Add retro font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        retro_computer: ['Retro_computer', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
