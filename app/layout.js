566a39bbce944966f60e36ddc521e86ab0f92e22
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-10 03:06:11 +0300

    Remove forntawesome

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer';
 
