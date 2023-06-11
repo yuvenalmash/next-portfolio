b9ac082f191abf2bfb5c18df0c3127b1b1acd855
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-11 19:22:58 +0300

    Change padding for small and medium screens

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Contact from '@/components/contact';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-16">
       <Headline />
       <Projects />
       <Techstack />
