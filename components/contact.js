c62fd9ab373a9b3c9a1dc79b529412ccc5ace0db
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-11 19:21:19 +0300

    Change background gradient

--- components/contact.js
+++ components/contact.js
@@ -13,27 +13,27 @@ const Contact = () => {
   };
   return (
     <div id="contact" className="py-20 flex flex-col justify-center items-center">
-      <h2 className="text-6xl font-boxpixies group text-white text-center mb-16 py-3 px-5 border">
+      <h2 className="flex text-6xl font-boxpixies group text-center mb-16 py-3 px-5 border border-slate-400 rounded">
         <span className="text-cyan-500 inline-block transition duration-300 ease-in-out group-hover:scale-110">C</span>
         ontact
       </h2>
-      <div className="container mx-auto">
+      <div className="container mx-auto w-fit">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-          <div className="bg-gradient-to-l from-transparent to-cyan-800 rounded-lg shadow-lg">
+          <div className="contact-item rounded shadow-lg w-fit pl-3">
             <div className="flex">
-              <a href={`mailto:${socialLinks.Email}`} className="text-white flex w-full justify-center gap-2 transition-all duration-300 ease-in-out hover:scale-125 hover:text-teal-500">
-                <h3 className="text-2xl font-bold text-white mb-4">Email</h3>
-                <AiOutlineMail className="text-4xl" />
+              <a href={`mailto:${socialLinks.Email}`} className="flex w-full justify-center gap-2 transition-all duration-300 ease-in-out hover:scale-125 hover:text-teal-500">
+                <h3 className="text-2xl">Email</h3>
+                <AiOutlineMail className="text-3xl" />
               </a>
             </div>
           </div>
 
           {Object.keys(socialsMap).map((key, index) => (
-            <div key={index} className="bg-gradient-to-l from-transparent to-cyan-800 rounded-lg shadow-lg">
+            <div key={index} className="contact-item rounded shadow-lg w-fit px-3">
               <div className="flex">
-                <a href={socialLinks[key]} target="_blank" rel="noopener noreferrer" className="text-white flex w-full  justify-center gap-2 transition-all duration-300 ease-in-out hover:scale-125 hover:text-teal-500">
-                  <h3 className="text-2xl font-bold text-white mb-4">{key}</h3>
-                  {React.createElement(socialsMap[key], { className: "text-4xl" })}
+                <a href={socialLinks[key]} target="_blank" rel="noopener noreferrer" className="flex w-full  justify-center gap-2 transition-all duration-300 ease-in-out hover:scale-125 hover:text-teal-500">
+                  <h3 className="text-2xl">{key}</h3>
+                  {React.createElement(socialsMap[key], { className: "text-3xl" })}
                 </a>
               </div>
             </div>
