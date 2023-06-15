2b38ff3b5dc73d0f455c26dd60391606b70010ad
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-15 15:42:50 +0300

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  output: 'export',
+  assetPrefix: isProd ? '/next-portfolio/' : '',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
