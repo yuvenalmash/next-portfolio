fda22c063253453cd9ad9e5037648707933c24e0
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-15 15:51:10 +0300

    Update settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
+  assetPrefix: isProd ? '/next-portfolio/' : '',
+  basePath: isProd ? '/next-portfolio' : '',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
