b3335063f040d81dc0829ca6ab9319ad3d2984b2
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-15 16:07:39 +0300

    update settings

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/next-portfolio' : '',
+  assetPrefix: isProd ? '/next-portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
