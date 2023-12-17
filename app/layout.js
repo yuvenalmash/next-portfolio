4870f997890fe6aaaa0c3fe7500a761c095ae6ec
[Yuvenal Njoroge] <[yuvenal.njoroge@gmail.com]>
2023-12-18 02:10:38 +0300

    Update portfolio description

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Yuvenal\'s Portfolio',
-  description: 'Portfolio of Yuvenal Njoroge, built with Next.js and Tailwind CSS',
+  description: 'Portfolio of Yuvenal Njoroge, a full stack developer based in Nairobi, Kenya.',
 }
 
 export default function RootLayout({ children }) {
