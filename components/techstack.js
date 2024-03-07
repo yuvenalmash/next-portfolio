613c6349b25b86ee55aec2eece7aaab77767cd39
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-03-08 00:20:42 +0300

    Apply SectionTitle component

--- components/techstack.js
+++ components/techstack.js
@@ -1,11 +1,34 @@
 import { techStackList } from "./portfolioData";
-import { DiPython, DiRuby, DiJavascript1, DiHtml5, DiCss3, DiDjango, DiReact, DiPostgresql, DiMysql, DiMongodb, DiGit, DiGithubBadge, DiHeroku } from "react-icons/di";
+import {
+  DiPython,
+  DiRuby,
+  DiJavascript1,
+  DiHtml5,
+  DiCss3,
+  DiDjango,
+  DiReact,
+  DiPostgresql,
+  DiMysql,
+  DiMongodb,
+  DiGit,
+  DiGithubBadge,
+  DiHeroku,
+} from "react-icons/di";
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiTailwindcss, SiSqlite, SiRuby, SiRubyonrails, SiVercel, SiJest, SiSelenium, SiNetlify, SiPostman} from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiSqlite,
+  SiRuby,
+  SiRubyonrails,
+  SiVercel,
+  SiJest,
+  SiSelenium,
+  SiNetlify,
+  SiPostman,
+} from "react-icons/si";
 import { GiCapybara } from "react-icons/gi";
 import { FcBriefcase } from "react-icons/fc";
-
-
+import SectionTitle from "./sectionTitle";
 
 const Techstack = () => {
   const techStackIcons = {
@@ -33,22 +56,27 @@ const Techstack = () => {
     Netlify: <SiNetlify className="text-2xl" />,
     Vercel: <SiVercel className="text-2xl" />,
     Postman: <SiPostman className="text-2xl" />,
-    Professional: <FcBriefcase className="text-2xl" />
+    Professional: <FcBriefcase className="text-2xl" />,
   };
 
   return (
-    <div id="tech-stack" className="py-20 flex flex-col justify-center items-center">
-      <h2 className="text-6xl font-boxpixies group text-center mb-16 py-3 px-5 border border-slate-400 shadow shadow-white">
-        <span className="text-cyan-500 inline-block transition duration-300 ease-in-out group-hover:scale-110">T</span>
-        ech Stack
-      </h2>
+    <div
+      id="tech-stack"
+      className="py-20 flex flex-col justify-center items-center"
+    >
+      <SectionTitle title="Tech Stack" />
       <p className="text-xl text-center mb-16 max-w-3xl">
-        I have worked with a variety of technologies in my career as a developer. Here are some of the technologies I have worked with and I&apos;m skilled at:
+        I have worked with a variety of technologies in my career as a
+        developer. Here are some of the technologies I have worked with and
+        I&apos;m skilled at:
       </p>
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-10 justify-center items-center">
           {Object.keys(techStackList).map((key, index) => (
-            <div key={index} className="tech-category shadow shadow-cyan-400 max-w-md hover:shadow-md hover:shadow-cyan-400  transition-all duration-300 ease-out">
+            <div
+              key={index}
+              className="tech-category shadow shadow-cyan-400 max-w-md hover:shadow-md hover:shadow-cyan-400  transition-all duration-300 ease-out"
+            >
               <div className="p-6">
                 <h3 className="text-2xl font-bold mb-4">{key}</h3>
                 <div className="flex flex-wrap gap-2 max-w-sm">
@@ -56,7 +84,9 @@ const Techstack = () => {
                     <div key={index} className="flex justify-center">
                       <span className="techStackItem text-slate-200 bg-cyan-950 px-2 py-1 rounded hover:scale-110 hover:bg-cyan-900 transition-colors duration-300 ease-in-out flex items-center space-x-2 gap-2">
                         <span className="techStachIcon inline-block transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                          {techStackIcons[tech] ? techStackIcons[tech] : techStackIcons["Professional"]}
+                          {techStackIcons[tech]
+                            ? techStackIcons[tech]
+                            : techStackIcons["Professional"]}
                         </span>
                         {tech}
                       </span>
